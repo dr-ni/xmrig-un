e8e871b9d2e3e8ba759f6746eabae9e7023fc2e5
dr-ni <niethammer@dr-niethammer.de>
2023-05-05 15:57:18 +0200

    un wallet

--- startshib.sh
+++ startshib.sh
@@ -1,2 +1,1 @@
-#sudo ./xmrig -t 16 -a rx -o stratum+ssl://rx.unmineable.com:443 -u SHIB:0x84e959876fd2cf1a4012056878898de4c18be1bd.upc -p x --asm intel --randomx-1gb-pages --cpu-affinity 0x000000000000FFFF 
-sudo ./xmrig -t 16 -a rx -o stratum+ssl://rx.unmineable.com:443 -u SHIB:0x5F554c887a3FeC679603cA27869Ce6A5Ab90d221.uwe -p x --asm intel --randomx-1gb-pages --cpu-affinity 0x000000000000FFFF
+sudo ./xmrig -t 16 -a rx -o stratum+ssl://rx.unmineable.com:443 -u SHIB:0x84e959876fd2cf1a4012056878898de4c18be1bd.upc -p x --asm intel --randomx-1gb-pages --cpu-affinity 0x000000000000FFFF 
